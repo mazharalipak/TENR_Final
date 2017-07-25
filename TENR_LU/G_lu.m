04a1a41b013728e78a0f36a19b761ecdb50ee176
Mazhar Ali <mazhar.ali@skolkovotech.ru>
2017-07-25 16:13:11 +0300

    Update G_lu.m

--- TENR_LU/G_lu.m
+++ TENR_LU/G_lu.m
@@ -13,30 +13,27 @@ New_UMat=bsxfun(@rdivide, U_Mat(1:end,:), diag(U_Mat));
 b=zeros(size(J,1),1);                            %% Right hand side ....(nth column of Identity matrix)
 b(end,1)=1;
 
-v=(New_UMat)\b;                    
+v=(New_UMat)\b;                                  %% The Right eigenvector in g_LU Transversality
 
-u=((L_Mat)')\b;
+u=((L_Mat)')\b;                                  %% The Left eigenvector in g_LU Transversality
 
-G=U_Mat(end,end);
+G=U_Mat(end,end);                                %% g_LU
   
 %% First option for this LU Transversality is based on Matlab LU decomposition.........................
 
 % [L_Mat, U_Mat, P_Mat, Q_Mat]=lu(J); 
 % New_UMat=bsxfun(@rdivide, U_Mat(1:end,:), diag(U_Mat));
-% 
-% 
+
 % [aa,bb]=min(abs(diag(U_Mat)));
-% 
+ 
 % b=zeros(size(J,1),1);                %% Right hand side ....(nth column of Identity matrix)
 % b(bb,1)=1;
-% 
-% v=(New_UMat)\b;                    
-% 
-% u=((L_Mat))'\b;
-% 
-% G=U_Mat(bb,bb);
-% 
-% E_M=sparse(eye(size(J)));
+ 
+% v=(New_UMat)\b;                      %% The Right eigenvector in g_LU Transversality             
+ 
+% u=((L_Mat))'\b;                      %% The Left eigenvector in g_LU Transversality  
+ 
+% G=U_Mat(bb,bb);                      %% g_LU
 
 
 end
